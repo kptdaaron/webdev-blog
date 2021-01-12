ee3a2671b93f2c299814d5d641f5df1ecf9537bb
kptdaaron <kptdaaron@gmail.com>
2021-01-12 15:02:07 +0800

    code cleanup and added footer form to blog-tid page

--- styles/style.js
+++ styles/style.js
@@ -3,21 +3,7 @@ const parallax = () => {
     $(".parallax").parallax();
   });
 };
-const carousel = () => {
-  $(".carousel").carousel(
-    {
-      height: 100,
-      width: 300,
-      padding: 200,
-    },
-    setTimeout(autoplay, 4500)
-  );
 
-  function autoplay() {
-    $(".carousel").carousel("next");
-    setTimeout(autoplay, 10000);
-  }
-};
 const sidenav = () => {
   $(document).ready(function () {
     $(".sidenav").sidenav();
@@ -25,5 +11,4 @@ const sidenav = () => {
 };
 
 sidenav();
-carousel();
 parallax();
